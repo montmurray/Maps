5f72ef82d9efe269dccec5c307d9ca4063451dc9
Tessa Murray <tmurray@bsd220.org>
2025-02-03 07:36:09 -0800

    Initial Commit

---
+++ Maps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Maps
+//
+//  Created by Tessa Murray on 2/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Maps/MapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapsApp.swift
+//  Maps
+//
+//  Created by Tessa Murray on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
