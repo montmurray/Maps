3bb169757bf735333914098e66edb4c2782429ab
Tessa Murray <tmurray@bsd220.org>
2025-02-03 07:49:05 -0800

    Added user location

---
+++ Maps/LocationManager.swift
@@ -0,0 +1,19 @@
+//
+//  LocationManager.swift
+//  Maps
+//
+//  Created by Tessa Murray on 2/3/25.
+//
+
+import CoreLocation
+import SwiftUI
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    var locationManager = CLLocationManager()
+    override init() {
+            super.init()
+            locationManager.delegate = self
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+}
